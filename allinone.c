81e6c6e66d86ef54d4f3947ea1637a9ee0ff996f
Zhouyi Zhou <zhouzhouyi@gmail.com>
2024-10-24 14:53:12 +0800

    Add inverse probability function

--- allinone.c
+++ allinone.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <gsl/gsl_sf_erf.h>
 
 double compute_alpha(double *data, int n, double xm) {
     double sum_log = 0.0;
@@ -78,13 +79,43 @@ double log_likelihood_pareto(double alpha, double xm, double *data, int n) {
     return log_likelihood;
 }
 
+double erfinv(double x) {
+    double w, p;
+    w = log((1.0 - x) * (1.0 + x));
+    p = 2.0 / (M_PI * 0.147) + 0.5 * w;
+    return (x * sqrt(sqrt(p * p - w / 0.147) - p));
+}
+
+double inverse_normal(double p, double mean, double stddev) {
+    return mean + stddev * sqrt(2) * erfinv(2 * p - 1);  // Quantile for normal distribution
+}
+
+float inverse_poisson(double p, double lambda) {
+    double cumulative_prob = 0.0;
+    float x = 0;
+    while (cumulative_prob < p) {
+        cumulative_prob += exp(-lambda) * pow(lambda, x) / tgamma(x + 1);
+        x++;
+    }
+    return x - 1;  // Return the smallest x such that cumulative probability >= p
+}
+
+double inverse_exp(double p, double lambda) {
+    return -log(1 - p) / lambda;  // Quantile for Exponential distribution
+}
+
+double inverse_pareto(double p, double alpha, double xm) {
+    return xm / pow(1 - p, 1 / alpha);  // Quantile for Pareto distribution
+}
+
+
 int main() {
     int n;
-    
+
     // Input number of elements
     // printf("Enter the number of elements: ");
     scanf("%d", &n);
-    
+
     // Check for valid input
     if (n <= 0) {
         printf("Number of elements must be positive.\n");
@@ -93,20 +124,24 @@ int main() {
 
     // Allocate memory to hold the elements
     double data[n];
-    
+
     // Input elements
     // printf("Enter the elements: ");
     for (int i = 0; i < n; i++) {
         scanf("%lf", &data[i]);
     }
-    
+
+    double probability;
+    printf("Enter probability (e.g., 0.95): ");
+    scanf("%lf", &probability);
+
     // Compute parameters
     double xm = compute_xm(data, n);
     double alpha = compute_alpha(data, n, xm);
     double mean = compute_mean(data, n);
     double stddev = compute_stddev(data, n, mean);
     double lambda = compute_lambda(data, n);
-    
+
     // Compute log-likelihoods
     double ll_exponential = log_likelihood_exponential(1.0 / mean, data, n);
     double ll_normal = log_likelihood_normal(mean, stddev, data, n);
@@ -128,23 +163,34 @@ int main() {
     printf("Poisson: %.5lf\n", ll_poisson);
     printf("Pareto: %.5lf\n", ll_pareto);
 
+    double normal_quantile = inverse_normal(probability, mean, stddev);
+    int poisson_quantile = inverse_poisson(probability, lambda);
+    double pareto_quantile = inverse_pareto(probability, alpha, xm);
+    double exp_quantile = inverse_exp(probability, 1.0/mean);
+
+
+
     double best_fit = ll_exponential;
     char *best_fit_name = "Exponential";
 
+    double quantile = exp_quantile;
     if (ll_normal > best_fit) {
         best_fit = ll_normal;
         best_fit_name = "Normal";
+	quantile = normal_quantile;
     }
     if (ll_poisson > best_fit) {
         best_fit = ll_poisson;
         best_fit_name = "Poisson";
+	quantile = poisson_quantile;
     }
     if (ll_pareto > best_fit) {
         best_fit = ll_pareto;
         best_fit_name = "Pareto";
+	quantile = pareto_quantile;
     }
 
-    printf("Best fit distribution: %s\n", best_fit_name);
+    printf("Best fit distribution: %s %f\n", best_fit_name, quantile);
 
     return 0;
 }
